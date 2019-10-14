c3c4e31dd6d574f371053205f26a90b3d3ae4dbb
chabotv <vincent.chabot@meteo.fr>
2019-10-14 13:11:11 +0200

    Changing the structure to be pack as a package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data_exploration",
+    version="0.0.1",
+    author="LabIA-MF",
+    author_email="gwennaelle.larvor@meteo.fr",
+    description="Data exploration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://gitlab.meteo.fr/deep_learning/data_exploration",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
