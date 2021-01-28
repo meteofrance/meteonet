7bdd199882e63ae26902237e31266373f35856fc
larvorg <gwennaelle.larvor@meteo.fr>
2021-01-28 18:05:48 +0100

    add files about satellite data : CT (Cloud Type)

--- meteonet_toolbox/constant.py
+++ meteonet_toolbox/constant.py
@@ -14,3 +14,40 @@ DOMAINS = {
     }
 
 
+#color palette about CT (Cloud Type) satellite data
+
+CLOUD_TYPE_16 = {
+    "0":"No data",
+    "1":"Cloud-free land",
+    "2":"Cloud-free sea",
+    "3":"Snow over land",
+    "4": "Sea ice",
+    "5":"Very low clouds",
+    "6":"Low clouds",
+    "7":"Mid-level clouds",
+    "8":"High opaque clouds",
+    "9":"Very high opaque clouds",
+    "10":"Fractional clouds",
+    "11":"High semi transparent thin clouds",
+    "12":"High semi transparent meanly thick clouds",
+    "13":"High semi transparent thick clouds",
+    "14":"High semi transparent above low or medium clouds",
+    "15":"High semi transparent above snow/ice"
+}
+
+CT_16_COLOR_MAP = [[0.39215686, 0.39215686, 0.39215686],
+                   [0., 0.47058824, 0.],
+                   [0., 0., 0.],
+                   [0.98039216, 0.74509804, 0.98039216],
+                   [0.8627451, 0.62745098, 0.8627451],
+                   [1., 0.39215686, 0.],
+                   [1., 0.70588235, 0.],
+                   [0.94117647, 0.94117647, 0.],
+                   [0.84313725, 0.84313725, 0.58823529],
+                   [0.90196078, 0.90196078, 0.90196078],
+                   [0.78431373, 0., 0.78431373],
+                   [0., 0.31372549, 0.84313725],
+                   [0., 0.70588235, 0.90196078],
+                   [0., 0.94117647, 0.94117647],
+                   [0.35294118, 0.78431373, 0.62745098],
+                   [1, 0.6, 1]]
